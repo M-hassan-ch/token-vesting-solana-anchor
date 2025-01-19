8e9f56f9a1a49ec0d05155e7e079f9a2777e8a20
Hassan <muhammad.hassan@antematter.io>
2025-01-20 02:54:47 +0500

    added create employee account functionality

--- anchor/programs/vesting/src/lib.rs
+++ anchor/programs/vesting/src/lib.rs
@@ -6,7 +6,7 @@ use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
-pub mod Vesting {
+pub mod vesting {
     use super::*;
 
     pub fn create_vesting_account(ctx: Context<CreateVestingAccount>, company_name: String) -> Result<()> {
@@ -18,6 +18,21 @@ pub mod Vesting {
             company_name,
             treasury_bump: ctx.bumps.token_treasury_account,
             bump: ctx.bumps.vesting_account
+        });
+        Ok(())
+    }
+
+    pub fn create_employee_account(ctx: Context<CreateEmployeeAccount>, start_date: u64, end_date: u64, cliff_date: u64, total_amount: u64) -> Result<()>{
+        let employee_account = &mut ctx.accounts.employee_account;
+        employee_account.set_inner(EmployeeAccount{
+            beneficiary: ctx.accounts.beneficiary.key(),
+            vesting_account: ctx.accounts.vesting_account.key(),
+            start_date,
+            end_date,
+            cliff_date,
+            total_amount,
+            total_withdrawn: 0,
+            bump: ctx.bumps.employee_account
         });
         Ok(())
     }
@@ -54,6 +69,29 @@ pub struct CreateVestingAccount<'info> {
     pub token_treasury_account: InterfaceAccount<'info, TokenAccount>,
 }
 
+#[derive(Accounts)]
+pub struct CreateEmployeeAccount<'info>{
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub beneficiary: SystemAccount<'info>,
+
+    #[account(
+        has_one = owner,
+    )]
+    pub vesting_account: Account<'info, VestingAccount>,
+
+    #[account(
+        init,
+        payer= owner,
+        space = 8 + EmployeeAccount::INIT_SPACE,
+        seeds = [b"employee_vesting", beneficiary.key().as_ref(), vesting_account.key().as_ref()],
+        bump
+    )]
+    pub employee_account: Account<'info, EmployeeAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
 #[account]
 #[derive(InitSpace)]
 pub struct VestingAccount {
@@ -63,5 +101,18 @@ pub struct VestingAccount {
     #[max_len(50)]
     company_name: String,
     treasury_bump: u8,
+    bump: u8,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct EmployeeAccount{
+    beneficiary: Pubkey,
+    vesting_account: Pubkey,
+    start_date: u64,
+    end_date: u64,
+    cliff_date: u64,
+    total_amount: u64,
+    total_withdrawn: u64,
     bump: u8,
 }
